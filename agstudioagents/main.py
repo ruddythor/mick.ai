a40ca15b13ee32754f7e1c3d1f12b0f64dd4348a
Josh B <joshlegs@gmail.com>
2024-05-04 18:25:27 -0400

    tweak llm sys prompts, other config

--- agstudioagents/main.py
+++ agstudioagents/main.py
@@ -2,15 +2,15 @@ import os
 import tempfile
 
 from autogen import ConversableAgent, UserProxyAgent, AssistantAgent, GroupChat, config_list_from_json, GroupChatManager
-from autogen.coding import LocalCommandLineCodeExecutor, CodeBlock
+from autogen.coding import LocalCommandLineCodeExecutor
 from rag import find_doc
 
 
 config_list = config_list_from_json(
     "OAI_CONFIG_LIST.json",
-    #filter_dict={"tags": ["llama3"]},  # comment out to get all
+    # filter_dict={"tags": ["llama3"]},  # comment out to get all
 )
-
+print("\n\n*******\nConfig_list is: ", config_list)
 # Create a temporary directory to store the code files.
 temp_dir = tempfile.TemporaryDirectory()
 
@@ -20,18 +20,14 @@ code_executor = LocalCommandLineCodeExecutor(
     work_dir="workspace",  # Use the temporary directory to store the code files.
     functions=[find_doc,]
 )
-# print(
-#     code_executor.execute_code_blocks(
-#         code_blocks=[
-#             CodeBlock(language="python", code="print('Hello, World!')"),
-#         ]
-#     )
-# )
+
 user = UserProxyAgent(
     "user",
     max_consecutive_auto_reply=10,
     system_message="""Your name is User. 
-        You are a helpful user proxy agent who can act on behalf of a user to provide feedback to other agents.
+        You are a helpful user proxy agent who can act on behalf of a user to provide feedback to other agents. 
+        You are able to END conversation chains when you see the phrase "TERMINATE" or "#####TERMINATE" in a response to you, 
+        although you can also continue the conversation if you feel your original request has not been addressed.
     """,
     llm_config={"config_list": config_list},
     human_input_mode="TERMINATE",
@@ -52,7 +48,7 @@ code_runner = AssistantAgent(
     "code_execution_agent",
     max_consecutive_auto_reply=10,
     system_message="""Your name is Code Executor. 
-        Your ONLY task is to execute code from a coding writer and report the results back.
+        Your ONLY task is to execute code from a coding writer and report the results back. You will always use skills from your functions library, accessible only to you.
     """,
     llm_config={"config_list": config_list},
     human_input_mode="NEVER",
@@ -60,6 +56,7 @@ code_runner = AssistantAgent(
         # 'work_dir': 'workspace',
         # 'use_docker': False,
         'last_n_messages': 15,
+        # 'work_dir': 'workspace',
         'executor': code_executor,
     },
     # is_termination_msg=lambda msg: "\n#####TERMINATE" in msg["content"].upper(),
@@ -81,7 +78,17 @@ In the following cases, suggest python code (in a python coding block) or shell 
 Solve the task step by step if you need to. If a plan is not provided, explain your plan first. Be clear which step uses code, and which step uses your language skill.
 When using code, you must indicate the script type in the code block. The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
 If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
-Always import your code from the 'functions' library.
+    3. Always import your code from the 'functions' library.
+    4. Never ever provide an isolated snippet of code, but instead you will provide full executable scripts, preferably in Python. Do not under any circumstances suggest another agent modify any code.
+    5. You can provide access to your skill, or tool, by providing code like the following to the user: 
+        ```python
+        from functions import find_doc
+        result = find_doc(query)
+        print(result)
+        ```
+        Where `query` is the argument you, the code_execution_agent, will pass into the function.
+    6. You have the following tools available to you: 
+        functions.find_doc
 """
 code_writer = ConversableAgent(
     "code_writer",
@@ -112,7 +119,6 @@ code_writer = ConversableAgent(
 # Register the tool function with the user proxy agent.
 #code_writer.register_for_execution(name="find_doc")(find_doc)
 
-
 def state_transition(last_speaker, groupchat):
     messages = groupchat.messages
 
@@ -121,7 +127,6 @@ def state_transition(last_speaker, groupchat):
         return code_writer
     elif last_speaker is code_writer:
         # retrieve: action 1 -> action 2
-#        return code_runner
         return code_runner
     elif last_speaker is code_runner:
         if "exitcode: 1" in messages[-1]["content"].lower():
@@ -134,10 +139,6 @@ def state_transition(last_speaker, groupchat):
         # research -> end
         return None
 
-# groupchat = GroupChat(
-#     speaker_selection_method=custom_speaker_selection_func,
-#     # ...,
-# )
 groupchat = GroupChat(
     agents=[user, code_writer, code_runner],
     messages=[],
@@ -148,7 +149,9 @@ groupchat = GroupChat(
 manager = GroupChatManager(groupchat=groupchat, llm_config={'config_list': config_list})
 
 # result = user.initiate_chat(
-#     code_writer, message="Please supply me with code for the executor to use given the query 'throawaylien and coach' for our find_doc tool/skill/function."
+#     code_writer, message="Please supply me with code for the executor to use given the query 'YOUR QUERY HERE' for our find_doc tool/skill/function."
 # )
-result = user.initiate_chat(manager, message="Please supply me with code for the executor to use given the query 'throawaylien and coach' for our find_doc tool/skill/function.")
-print(result)
+while True:
+    query = input("Please enter your local docs query: \n")
+    result = user.initiate_chat(manager, message=f"Please supply me with code for the executor to use given the following query: \"{query}\"")
+    print(result)
