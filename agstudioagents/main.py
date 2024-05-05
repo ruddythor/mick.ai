5378e77ad13ae0073fc0dbf5a7443cd26f072459
Josh B <joshlegs@gmail.com>
2024-05-05 11:06:52 -0400

    make localdocs search more usable

--- agstudioagents/main.py
+++ agstudioagents/main.py
@@ -26,18 +26,18 @@ user = UserProxyAgent(
     max_consecutive_auto_reply=10,
     system_message="""Your name is User. 
         You are a helpful user proxy agent who can act on behalf of a user to provide feedback to other agents. 
-        You are able to END conversation chains when you see the phrase "TERMINATE" or "#####TERMINATE" in a response to you, 
+        You are able to END conversation chains when you see the phrase "#####TERMINATE" by responding only with the phrase "#####TERMINATE", 
         although you can also continue the conversation if you feel your original request has not been addressed.
     """,
     llm_config={"config_list": config_list},
-    human_input_mode="ALWAYS",
+    human_input_mode="NEVER",
     code_execution_config=False, #{
         # 'work_dir': 'workspace',
         # 'use_docker': False,
         # 'last_n_messages': 15,
         # 'executor': code_executor,
     # },
-    is_termination_msg=lambda msg: "\n#####TERMINATE" in msg["content"].upper(),
+    is_termination_msg=lambda msg: "\n#####TERMINATE" in msg["content"],
     default_auto_reply='I did not execute any code or generate any output. Please rephrase your response so that I can better understand.',
     description='A user-proxy agent to act on behalf of the user and relay results back to other agents.'
     # max_consecutive_auto_reply=
@@ -50,6 +50,13 @@ code_runner = AssistantAgent(
     system_message="""Your name is Code Executor. 
         Your ONLY task is to execute code from a coding writer and report the results back. You will always use skills from your functions library, accessible only to you.
         You will report ONLY the results of the code back, and you will never interpet or conjecture about the results.
+
+        To use your find_doc skill, you would import it from functions, like so:
+        ```python
+        from functions import find_doc
+        result = find_doc(query)
+        print result.summary
+        ```
     """,
     llm_config={"config_list": config_list},
     human_input_mode="NEVER",
@@ -70,9 +77,30 @@ code_runner = AssistantAgent(
 # Register the tool function with the user proxy agent.
 code_runner.register_for_execution(name="find_doc")(find_doc)
 
+analyzer = AssistantAgent(
+    "analysis_agent",
+    max_consecutive_auto_reply=10,
+    system_message="""Your name is Analysis Agent. 
+        Your ONLY task is to make sure that you provide a concise but effective summary of any input you receive. 
+        You can also provide suggestions for next steps based on the context of the entire conversation. 
+        You are also able to make logical jumps to infer facts or future events, but you are always explicit to call out when you are making logical leaps.
+
+        It is critical that, when you feel the task has been accomplished or the request has been satisifed, you reply with the phrase "#####TERMINATE" and only that.
+    """,
+    llm_config={"config_list": config_list},
+    human_input_mode="ALWAYS",
+    code_execution_config=False, 
+    default_auto_reply='I did not understand your request. Please try again.',
+    description='An Analysis Agent dedicated to analyzing the output of any other agent.'
+    # max_consecutive_auto_reply=
+    #is_termination_msg=lambda msg: "\n#####TERMINATE" in msg["content"].upper(),
+)
+
+# Register the tool function with the user proxy agent.
+#analyzer.register_for_execution(name="find_doc")(find_doc)
 
 write_sys_msg = """
-You have been given coding capability to solve tasks using Python code.
+You have been given exceptional coding capability to solve tasks using Python code. In fact, you are actually a Python god, and can write extremely performant, concise and readable Python code.
 In the following cases, suggest python code (in a python coding block) or shell script (in a sh coding block) for the user to execute.
     1. When you need to collect info, use the code to output the info you need, for example, browse or search the web, download/read a file, print the content of a webpage or a file, get the current date/time, check the operating system. After sufficient info is printed and the task is ready to be solved based on your language skill, you can solve the task by yourself.
     2. When you need to perform some task with code, use the code to perform the task and output the result. Finish the task smartly.
@@ -85,7 +113,7 @@ If you want the user to save the code in a file before executing it, put # filen
         ```python
         from functions import find_doc
         result = find_doc(query)
-        print(result)
+        print(result.summary)
         ```
         Where `query` is the argument you, the code_execution_agent, will pass into the function.
     6. You have the following tools available to you: 
@@ -125,13 +153,15 @@ def state_transition(last_speaker, groupchat):
             return code_writer
         else:
             # retrieve --(execution success)--> research
-            return user
+            return analyzer
+    elif last_speaker is analyzer:
+        return user
     elif last_speaker == "user":
         # research -> end
         return None
 
 groupchat = GroupChat(
-    agents=[user, code_writer, code_runner],
+    agents=[user, code_writer, code_runner, analyzer],
     messages=[],
     max_round=10,
     speaker_selection_method=state_transition,
@@ -145,4 +175,6 @@ manager = GroupChatManager(groupchat=groupchat, llm_config={'config_list': confi
 while True:
     query = input("Please enter your local docs query: \n")
     result = user.initiate_chat(manager, message=f"Please supply me with code for the executor to use given the following query: \"{query}\"")
-    print(result)
+    print("\n----------\nCHAT HISTORY:\n", result.chat_history)
+    print("\n\n===================\nSUMMARY: ", result.summary)
+    
